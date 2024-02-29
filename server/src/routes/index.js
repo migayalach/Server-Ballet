e08524acae886770d3c006881993ae43f2e1fb0a
migayalach <ayalachavezmiguel@gmail.com>
2024-02-28 20:51:59 -0400

    entry points listos

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,20 @@
 const { Router } = require("express");
 const mainRouter = Router();
 
+// ROUTES
+const assistanceRouter = require("./assistanceRoute");
+const classRouter = require("./classRoute");
+const levelRouter = require("./levelRoute");
+const paymentRouter = require("./paymentRoute");
+const qualificationRouter = require("./qualificationRoute");
+const userRouter = require("./userRoute");
+
+// ENTRY POINT'S
+mainRouter.use("/asistance", assistanceRouter);
+mainRouter.use("/class", classRouter);
+mainRouter.use("/level", levelRouter);
+mainRouter.use("/payment", paymentRouter);
+mainRouter.use("/qualification", qualificationRouter);
+mainRouter.use("/user", userRouter);
+
 module.exports = mainRouter;
