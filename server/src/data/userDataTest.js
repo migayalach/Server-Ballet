bf836aa21ce6115ebfb491e5137797ec0ed5f177
migayalach <ayalachavezmiguel@gmail.com>
2024-03-07 23:07:58 -0400

    test

--- server/src/data/userDataTest.js
+++ server/src/data/userDataTest.js
@@ -1,67 +1,74 @@
 // LEVEL 1 = TEACHER
 const user1 = {
+  idUser: 10,
   idLevel: 1,
+  idExtension: 1,
   nameUser: "Miguel",
   emailUser: "ayalachavez@gmail.com",
   carnet: 9879,
-  extension: "LP",
   registrationNumber: "AM-123",
 };
 
 // LEVEL 2 = STUDENT
 const user2 = {
+  idUser: "10",
   idLevel: 2,
+  idExtension: "12",
   nameUser: "Lizz Larico Flores",
   emailUser: "lizzFlores@gmail.com",
   carnet: 2051,
-  extension: "LP",
   registrationNumber: "LFL-204",
 };
 
 const user3 = {
+  idUser: "",
   idLevel: "holis",
+  idExtension: "",
   nameUser: "Mireya Pereira Ch.",
   emailUser: "",
-  carnet,
-  extension,
+  carnet: "",
   registrationNumber: "PCM-501",
 };
 
 const user4 = {
+  idUser: 123456789,
   idLevel: 2,
+  idExtension: 1,
   nameUser: "",
   emailUser: "emphy@gmail.com",
   carnet: "CA12Na",
-  extension: "OR",
   registrationNumber: "PCM-501",
 };
 
 const user5 = {
+  idUser: 100,
   idLevel: 2,
+  idExtension: 8,
   nameUser: "Emphy Mamani Perez",
   emailUser: "emphy@gmail.com",
   carnet: 55760,
-  extension: "",
   registrationNumber: 21321,
 };
 
 const user6 = {
+  idUser: "",
   idLevel: 2,
+  idExtension: 4,
   nameUser: "Emphy Mamani Perez",
   emailUser: "emphy@gmail.com",
   carnet: "",
-  extension,
   registrationNumber: "",
 };
 
 // DATOS REPETIDOS
 const user7 = {
+  idUser: "",
   idLevel: 2,
+  idExtension: 9,
   nameUser: "Lizz Larico Flores",
-  emailUser: "lizzFlores@gmail.com",
-  carnet,
-  extension,
-  registrationNumber: "LFL-204",
+  emailUser: "",
+  carnet: "",
+  registrationNumber: "",
 };
 
 module.exports = { user1, user2, user3, user4, user5, user6, user7 };
