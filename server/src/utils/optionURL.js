0a757cc3c7cd93ef5a6d5f74a01ced4e771e3fee
migayalach <ayalachavezmiguel@gmail.com>
2024-03-14 22:17:30 -0400

    se agrego un nuevo caso en este caso staff

--- server/src/utils/optionURL.js
+++ server/src/utils/optionURL.js
@@ -2,6 +2,10 @@ function URL_OPTION(option) {
   switch (option) {
     case "level":
       return `level?page=`;
+    case "extension":
+      return `extension?page=`;
+    case "staff":
+      return `staff?page=`;
   }
 }
 
