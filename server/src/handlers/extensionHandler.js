f3f1d4005848e9731c8dfb2e8b1d0f87ef5cfb9e
migayalach <ayalachavezmiguel@gmail.com>
2024-03-18 22:12:04 -0400

    handler completo

--- server/src/handlers/extensionHandler.js
+++ server/src/handlers/extensionHandler.js
@@ -6,40 +6,40 @@ const {
   removeExtension,
 } = require("../controllers/extensionController");
 
-const postExtension = (request, response) => {
+const postExtension = async (request, response) => {
   const { nameExtension } = request.body;
   try {
-    const data = createExtension(nameExtension);
+    const data = await createExtension(nameExtension);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
 };
 
-const getExtensionAll = (request, response) => {
+const getExtensionAll = async (request, response) => {
   const { page } = request.query;
   try {
-    const data = !page ? getAllExtension() : getPageExtension(page);
+    const data = !page ? await getAllExtension() : await getPageExtension(page);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
 };
 
-const putExtension = (request, response) => {
+const putExtension = async (request, response) => {
   const { idExtension, nameExtension } = request.body;
   try {
-    const data = updateExtension(idExtension, nameExtension);
+    const data = await updateExtension(idExtension, nameExtension);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
 };
 
-const deleteExtension = (request, response) => {
+const deleteExtension = async (request, response) => {
   const { idExtension } = request.params;
   try {
-    const data = removeExtension(idExtension);
+    const data = await removeExtension(idExtension);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
