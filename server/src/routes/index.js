c2a8a2ed971b84db0d765c868a3c694e32a8703e
migayalach <ayalachavezmiguel@gmail.com>
2024-03-18 22:04:31 -0400

    rouer hours importada y con entry point listo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,23 +4,25 @@ const mainRouter = Router();
 // ROUTES
 const assistanceRouter = require("./assistanceRoute");
 const levelRouter = require("./levelRoute");
+const extensionRouter = require("./extensionRoute");
 const staffRouter = require("./staffRouter");
+const hoursRouter = require("./hoursRoute");
 
 // const classRouter = require("./classRoute");
 // const paymentRouter = require("./paymentRoute");
 // const qualificationRouter = require("./qualificationRoute");
 // const userRouter = require("./userRoute");
-// const extensionRouter = require("./extensionRoute");
 
 // ENTRY POINT'S
 mainRouter.use("/level", levelRouter);
 mainRouter.use("/asistance", assistanceRouter);
+mainRouter.use("/extension", extensionRouter);
 mainRouter.use("/staff", staffRouter);
+mainRouter.use("/hours", hoursRouter);
 
 // mainRouter.use("/class", classRouter);
 // mainRouter.use("/payment", paymentRouter);
 // mainRouter.use("/qualification", qualificationRouter);
 // mainRouter.use("/user", userRouter);
-// mainRouter.use("/extension", extensionRouter);
 
 module.exports = mainRouter;
