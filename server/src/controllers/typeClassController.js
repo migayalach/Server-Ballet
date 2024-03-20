41f47bb9ed2ca8db7338ac6d566e9310698c7f0e
migayalach <ayalachavezmiguel@gmail.com>
2024-03-19 21:44:04 -0400

    typeclass controller en proceso

---
+++ server/src/controllers/typeClassController.js
@@ -0,0 +1,20 @@
+const createTypeClass = () => {};
+
+const getAllTypeClass = () => {};
+
+const getPageTypeClass = () => {};
+
+const getIdTypeClass = () => {};
+
+const updateTypeClass = () => {};
+
+const removeTypeClass = () => {};
+
+module.exports = {
+  createTypeClass,
+  getAllTypeClass,
+  getPageTypeClass,
+  getIdTypeClass,
+  updateTypeClass,
+  removeTypeClass,
+};
