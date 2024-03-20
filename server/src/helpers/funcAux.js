f5816d8513e115533e4a9d18e4320a24d52fba71
migayalach <ayalachavezmiguel@gmail.com>
2024-03-19 21:39:53 -0400

    se agrego nuevas funciones

--- server/src/helpers/funcAux.js
+++ server/src/helpers/funcAux.js
@@ -1,30 +1,23 @@
-function isNumber(idLevel) {
-  if (typeof idLevel === "number") {
-    return false;
+function isNumber(idElement) {
+  if (isNaN(idElement)) {
+    return true;
   }
-  if (idLevel % 1 !== 0) {
-    return false;
-  }
-  return true;
+  return false;
 }
 
-function lengthNameLevel(nameLevel) {
+function isString(idElement) {
+  if (typeof idElement === "string") {
+    return true;
+  }
+  return false;
+}
+
+function lengthName(nameLevel) {
   if (!nameLevel.length) {
     return false;
   }
   return true;
 }
-
-function toNumber(idLevel) {
-  if (typeof idLevel === "string") {
-    return false;
-  }
-  if (idLevel % 1 !== 0) {
-    return false;
-  }
-  return true;
-}
-
 
 function lengthElderForElementents(nameExtension) {
   if (nameExtension.length > 4) {
@@ -33,9 +26,44 @@ function lengthElderForElementents(nameExtension) {
   return true;
 }
 
+function levelExist(idLevel) {
+  if (idLevel) {
+    return false;
+  }
+  return true;
+}
+
+function extensionExist(idExtension) {
+  if (idExtension) {
+    return false;
+  }
+  return true;
+}
+
+function userRepeated(carnetStaff) {
+  if (carnetStaff) {
+    return false;
+  }
+  return true;
+}
+
+function dateComplete(startTime, endTime, totalTime) {
+  if (isString(startTime) && isString(endTime) && isString(totalTime)) {
+    if (startTime.length && endTime.length && totalTime.length) {
+      return true;
+    }
+    throw Error("Solo se aceptan horas completas");
+  }
+  throw Error(`Por favor ingrese las horas solicitadas`);
+}
+
 module.exports = {
   isNumber,
-  lengthNameLevel,
-  toNumber,
+  isString,
+  lengthName,
   lengthElderForElementents,
+  levelExist,
+  extensionExist,
+  userRepeated,
+  dateComplete
 };
