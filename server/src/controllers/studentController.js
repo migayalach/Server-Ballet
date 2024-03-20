25a98f46b7956ca88b646afd8d3144a502bd9a8f
migayalach <ayalachavezmiguel@gmail.com>
2024-03-20 10:01:05 -0400

    maquedato de controller studente

---
+++ server/src/controllers/studentController.js
@@ -0,0 +1,41 @@
+const responseData = require("../utils/response");
+const { isNumber, lengthName, isString } = require("../helpers/funcAux");
+
+const getAllStudent = () => {
+  return;
+};
+
+const getIdStudent = (idUser) => {
+  return;
+};
+
+const getPageStudent = (page) => {};
+
+const createStudent = (
+  idLevel,
+  idExtension,
+  nameStudent,
+  lastNameStudent,
+  emailStudent,
+  carnetStudent,
+  addressStudent,
+  dateBirthStudent,
+  photoStudent
+) => {};
+
+const updateStudent = () => {
+  return;
+};
+
+const removeStudent = (idUser) => {
+  return;
+};
+
+module.exports = {
+  createStudent,
+  getAllStudent,
+  getIdStudent,
+  getPageStudent,
+  updateStudent,
+  removeStudent,
+};
