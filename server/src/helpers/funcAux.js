b988907f270b28acb5426079cd6afb666ff6eb48
migayalach <ayalachavezmiguel@gmail.com>
2024-03-20 20:39:25 -0400

    se sumaron nuevas funciones

--- server/src/helpers/funcAux.js
+++ server/src/helpers/funcAux.js
@@ -57,6 +57,34 @@ function dateComplete(startTime, endTime, totalTime) {
   throw Error(`Por favor ingrese las horas solicitadas`);
 }
 
+function codeStaffStudent(lastName, name, carnet) {
+  const nameLast = lastName.split(" ");
+  const codeStudent = `${nameLast[0].slice(0, 2)}${nameLast[1].slice(
+    0,
+    2
+  )}${name.slice(0, 2)}${carnet.toString().slice(0, 2)}`.toUpperCase();
+  return codeStudent;
+}
+
+function completeStaffStudent(idExtension, name, lastName, email, carnet) {
+  if (isNumber(idExtension) || isString(idExtension)) {
+    throw Error(`Por favor asigne una extension`);
+  }
+  if (!isString(name) || !lengthName(name)) {
+    throw Error(`Por introduzca el nombre del estudiante`);
+  }
+  if (!isString(lastName) || !lengthName(lastName)) {
+    throw Error(`Por favor introduzca los apellidos del estudiante`);
+  }
+  if (!isString(email) || !lengthName(email)) {
+    throw Error(`Por favor introduzca el email del estudiante`);
+  }
+  if (isNumber(carnet) || isString(carnet)) {
+    throw Error(`Por favor introduzca el numero de carnet`);
+  }
+  return true;
+}
+
 module.exports = {
   isNumber,
   isString,
@@ -65,5 +93,7 @@ module.exports = {
   levelExist,
   extensionExist,
   userRepeated,
-  dateComplete
+  dateComplete,
+  codeStaffStudent,
+  completeStaffStudent,
 };
