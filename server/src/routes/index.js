4fc25d528f07405ff5214388ae8109171209b6a2
migayalach <ayalachavezmiguel@gmail.com>
2024-03-21 22:54:36 -0400

    nueva ruta class

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,28 +2,28 @@ const { Router } = require("express");
 const mainRouter = Router();
 
 // ROUTES
-const assistanceRouter = require("./assistanceRoute");
 const levelRouter = require("./levelRoute");
 const extensionRouter = require("./extensionRoute");
-const staffRouter = require("./staffRouter");
 const hoursRouter = require("./hoursRoute");
 const typeClassRouter = require("./typeClassRoute");
 const studentRouter = require("./studentRoute");
+const staffRouter = require("./staffRouter");
+const classRouter = require("./classRoute");
 
-// const classRouter = require("./classRoute");
+// const assistanceRouter = require("./assistanceRoute");
 // const paymentRouter = require("./paymentRoute");
 // const qualificationRouter = require("./qualificationRoute");
 
 // ENTRY POINT'S
 mainRouter.use("/level", levelRouter);
-mainRouter.use("/asistance", assistanceRouter);
 mainRouter.use("/extension", extensionRouter);
-mainRouter.use("/staff", staffRouter);
 mainRouter.use("/hours", hoursRouter);
 mainRouter.use("/typeClass", typeClassRouter);
 mainRouter.use("/student", studentRouter);
+mainRouter.use("/staff", staffRouter);
+mainRouter.use("/class", classRouter);
 
-// mainRouter.use("/class", classRouter);
+// mainRouter.use("/asistance", assistanceRouter);
 // mainRouter.use("/payment", paymentRouter);
 // mainRouter.use("/qualification", qualificationRouter);
 
