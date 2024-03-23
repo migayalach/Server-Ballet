56c3febec48cfd5b622111fd6c63b6778a7bd494
migayalach <ayalachavezmiguel@gmail.com>
2024-03-22 22:48:30 -0400

    se agrego la nueva ruta que es login

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,6 +9,7 @@ const typeClassRouter = require("./typeClassRoute");
 const studentRouter = require("./studentRoute");
 const staffRouter = require("./staffRouter");
 const classRouter = require("./classRoute");
+const loginRouter = require("./loginRoute");
 
 // const assistanceRouter = require("./assistanceRoute");
 // const paymentRouter = require("./paymentRoute");
@@ -22,9 +23,10 @@ mainRouter.use("/typeClass", typeClassRouter);
 mainRouter.use("/student", studentRouter);
 mainRouter.use("/staff", staffRouter);
 mainRouter.use("/class", classRouter);
+mainRouter.use("/login", loginRouter)
 
-// mainRouter.use("/asistance", assistanceRouter);
 // mainRouter.use("/payment", paymentRouter);
+// mainRouter.use("/asistance", assistanceRouter);
 // mainRouter.use("/qualification", qualificationRouter);
 
 module.exports = mainRouter;
