1be30609bdb63d08febe032580471ca6fec48df9
migayalach <ayalachavezmiguel@gmail.com>
2024-04-18 23:05:35 -0400

    calificacion handler

---
+++ server/src/handlers/qualificationHandler.js
@@ -0,0 +1,62 @@
+const {
+  createQualification,
+  // getAllStaff,
+  // getPageStaff,
+  // getIdStaff,
+  // updateStaff,
+  // removeStaff,
+} = require("../controllers/../controllers/qualificationController");
+
+const postQualification = async (request, response) => {
+  const { idClass, idStudent, notes, average } = request.body;
+  try {
+    const data = await createQualification(idClass, idStudent, notes, average);
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const getQualificationAll = (request, response) => {
+  try {
+    const data = 1;
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const getQualificationId = (request, response) => {
+  try {
+    const data = 1;
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const putQualification = (request, response) => {
+  try {
+    const data = 1;
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+const deleteQualification = (request, response) => {
+  try {
+    const data = 1;
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  postQualification,
+  getQualificationAll,
+  getQualificationId,
+  putQualification,
+  deleteQualification,
+};
