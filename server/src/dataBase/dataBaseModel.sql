1fb918e1df5ae4447ac5c4f5220d8622802a22a3
migayalach <ayalachavezmiguel@gmail.com>
2024-04-30 22:33:28 -0400

    databasemodel

---
+++ server/src/dataBase/dataBaseModel.sql
@@ -0,0 +1,76 @@
+create database balletJessica
+
+use balletJessica
+
+-- OK
+create table level(
+  idLevel int auto_increment not null,
+  nameLevel varchar(100) not null,
+  primary key(idLevel)
+);
+
+-- OK
+create table extension(
+  idExtension int auto_increment not null,
+  department varchar (4) unique not null,
+  primary key(idExtension) 
+);
+
+-- OK
+create table user(
+  idUser int auto_increment not null,
+  idLevel int not null,
+  idExtension int not null,
+  nameUser varchar(100) not null,
+  lastNameUser varchar(100) not null,
+  emailUser varchar(100) unique not null,
+  passwordUser varchar(100) not null,
+  addressUser varchar(500),
+  dateBirthUser date,
+  carnetUser int unique not null,
+  photoUser text,
+  stateUser boolean default true not null,
+  primary key(idUser),
+  foreign key(idLevel) references level(idLevel),
+  foreign key(idExtension) references extension(idExtension)
+);
+
+-- OK
+create table hours(
+  idHours int auto_increment not null,
+  startTime time not null,
+  endTime time not null,
+  totalTime time not null,
+  stateHours boolean default true not null,
+  primary key(idHours)
+);
+
+-- OK
+create table typeClass(
+  idTypeClass int auto_increment not null,
+  nameClass varchar(100) not null,
+  description varchar(255),
+  primary key(idTypeClass)
+);
+
+-- OK
+create table class(
+  idClass int auto_increment not null,
+  idHours int not null,
+  idUser int not null,
+  idTypeClass int not null,
+  parallel varchar(8) not null,
+  stateClass boolean default true not null,
+  primary key(idClass),
+  foreign key(idHours) references hours(idHours),
+  foreign key(idUser) references user(idUser),
+  foreign key(idTypeClass) references typeClass(idTypeClass)
+);
+
+-- OK
+create table student(
+  idClass int not null,
+  idUser int not null,
+  foreign key(idClass) references class(idClass),
+  foreign key(idUser) references user(idUser)
+);
