0be88f0cdf989d84d9000b353880fe7b183ecab3
migayalach <ayalachavezmiguel@gmail.com>
2024-05-04 23:53:47 -0400

    current pages

--- server/src/utils/currentPages.js
+++ server/src/utils/currentPages.js
@@ -2,13 +2,14 @@ function pagesCurrent(count, elementPage) {
   return Math.ceil(count / elementPage);
 }
 
+// TODO ARREGLAR ESTA LINEA PORQUE CUANDO SE HACE EL SALGO DE EJEMPLO 3 A 2 DEVUELVE THERE IS NOTHING HERE
 function pageCurrent(page, pages) {
   if (page <= 1) {
     return 1;
-  } else if (page > pages) {
-    throw Error(`There is nothing here`);
+  } else if (page <= pages) {
+    return page;
   }
-  return page;
+  throw Error(`There is nothing here`);
 }
 
 module.exports = {
