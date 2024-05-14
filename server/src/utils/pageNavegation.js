bf348d4dc8d85fef4549b1a5e4488e89da5e6b52
migayalach <ayalachavezmiguel@gmail.com>
2024-05-13 21:12:56 -0400

    page navegation

--- server/src/utils/pageNavegation.js
+++ server/src/utils/pageNavegation.js
@@ -2,9 +2,24 @@ require("dotenv").config();
 const { URL } = process.env;
 const URL_OPTION = require("./optionURL");
 
+function queryIdObject(queryId, page, operation) {
+  let search = "";
+  for (let i in queryId) {
+    if (queryId[i]) {
+      search += `${i}=${queryId[i]}&`;
+    }
+  }
+  return operation === "+"
+    ? `${URL}filter?${search}page=${page + 1}`
+    : `${URL}filter?${search}page=${page - 1}`;
+}
+
 function nextPage(direction, page, pages, queryId) {
   if (pages === 1 || pages === page) {
     return null;
+  }
+  if (typeof queryId === "object") {
+    return queryIdObject(queryId, page, "+");
   }
   const request = URL_OPTION(direction, queryId);
   return `${URL}${request}${page + 1}`;
@@ -14,6 +29,9 @@ function prevPage(direction, page, queryId) {
   const pagePrev = page - 1;
   if (pagePrev === 0) {
     return null;
+  }
+  if (typeof queryId === "object") {
+    return queryIdObject(queryId, page, "-");
   }
   const request = URL_OPTION(direction, queryId);
   return `${URL}${request}${page - 1}`;
