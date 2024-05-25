44019544fedf9e29e2d2d19f2b8b258c45a1e702
migayalach <ayalachavezmiguel@gmail.com>
2024-05-24 23:25:27 -0400

    login controller

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -1,10 +1,11 @@
 const bcrypt = require("bcrypt");
 const pool = require("../dataBase/conexion");
+const { getIdUser } = require("../controllers/userController");
 
 // TODO ACCESO LOGIN
 const loginAccess = async (email, password) => {
   const [data] = await pool.query(
-    `SELECT s.nameUser, s.lastNameUser, s.emailUser, s.passwordUser, l.nameLevel FROM user s, level l WHERE s.idLevel = l.idLevel AND s.emailUser = ? `,
+    `SELECT s.idUser, s.nameUser, s.lastNameUser, s.emailUser, s.passwordUser, l.nameLevel FROM user s, level l WHERE s.idLevel = l.idLevel AND s.emailUser = ? `,
     [email]
   );
   if (!data.length) {
@@ -18,6 +19,7 @@ const loginAccess = async (email, password) => {
     access: true,
     user: `${data[0].nameUser} ${data[0].lastNameUser}`,
     level: `${data[0].nameLevel}`,
+    dataUser: await getIdUser(data[0].idUser),
   };
 };
 
