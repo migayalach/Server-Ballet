354567e81eb3c36c24ed735af42fba622fb18948
migayalach <ayalachavezmiguel@gmail.com>
2024-05-28 23:32:28 -0400

    filter handler

--- server/src/handlers/filterHandler.js
+++ server/src/handlers/filterHandler.js
@@ -1,7 +1,8 @@
-const { filterUser } = require("../controllers/filterController");
+const { filterUser, filterAll } = require("../controllers/filterController");
 
 const getFilterUser = async (request, response) => {
   const {
+    all,
     search,
     order,
     nameOrLastName,
@@ -16,20 +17,23 @@ const getFilterUser = async (request, response) => {
     page,
   } = request.query;
   try {
-    const data = await filterUser(
-      search,
-      order,
-      nameOrLastName,
-      idLevel,
-      idExtension,
-      stateUser,
-      totalTime,
-      stateHours,
-      idUser,
-      idTypeClass,
-      stateClass,
-      page
-    );
+    const data = !all
+      ? await filterUser(
+          all,
+          search,
+          order,
+          nameOrLastName,
+          idLevel,
+          idExtension,
+          stateUser,
+          totalTime,
+          stateHours,
+          idUser,
+          idTypeClass,
+          stateClass,
+          page
+        )
+      : await filterAll(all);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
