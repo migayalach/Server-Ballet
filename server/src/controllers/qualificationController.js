6665cdfcdb0053e38bd177a0ee648d049938539f
migayalach <ayalachavezmiguel@gmail.com>
2024-06-05 16:14:13 -0400

    qualification controll

--- server/src/controllers/qualificationController.js
+++ server/src/controllers/qualificationController.js
@@ -2,8 +2,12 @@ const responseData = require("../utils/response");
 
 const pool = require("../dataBase/conexion");
 
-const createQualification = async (idClass, idStudent, notes, average) => {
-  await pool.query("INSERT INTO ")  
+const createQualification = async (idClass, idStudent, qualification) => {
+  // await pool.query("INSERT INTO ")
+  console.log(idClass);
+  console.log(idStudent);
+  console.log(qualification);
+  return;
 };
 
 module.exports = {
