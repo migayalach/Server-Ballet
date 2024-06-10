e6773985cb44b9e577895e1d21e585831672098e
migayalach <ayalachavezmiguel@gmail.com>
2024-06-09 23:43:54 -0400

    qualification controllers

--- server/src/controllers/qualificationController.js
+++ server/src/controllers/qualificationController.js
@@ -1,21 +1,46 @@
 const responseData = require("../utils/response");
-
 const pool = require("../dataBase/conexion");
+const { getIdUser } = require("./userController");
 
 const createQualification = async (idClass, idStudent, qualification) => {
   // await pool.query("INSERT INTO ")
-  console.log(idClass);
-  console.log(idStudent);
-  console.log(qualification);
-  return;
+  // console.log(idClass);
+  // console.log(idStudent);
+  // console.log(qualification);
+  // return;
 };
 
 const getAllStaff = async () => {
-  const [data] = await pool.query(`SELECT * FROM qualification`);
-  return data;
+  // const [data] = await pool.query(`SELECT * FROM qualification`);
+  // return data;
+};
+
+const getIdQualificationAll = async (idUser) => {
+  const page = 1;
+  const { nameLevel } = await getIdUser(idUser);
+  if (nameLevel === "Estudiante" || nameLevel === "Secretaria") {
+    throw Error(`Lo siento usted no tiene permiso para acceder`);
+  }
+  let query =
+    "SELECT p.idParams, p.idClass, c.parallel, u.nameUser, u.lastNameUser, p.dateTest, p.title FROM params p, class c, user u WHERE p.idClass = c.idClass AND u.idUser = c.idUser ";
+  if (nameLevel === "Director") {
+    const [data] = await pool.query(query);
+    if (!data.length) {
+      throw Error`No se encontraron clases`;
+    }
+    return responseData(data, "qualificationList", page);
+  } else if (nameLevel === "Profesor") {
+    query += `AND c.idUser = ${idUser}`;
+    const [data] = await pool.query(query);
+    if (!data.length) {
+      throw Error`No se encontraron clases`;
+    }
+    return responseData(data, "qualificationList", page);
+  }
 };
 
 module.exports = {
   createQualification,
   getAllStaff,
+  getIdQualificationAll,
 };
