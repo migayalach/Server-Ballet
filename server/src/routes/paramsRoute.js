6d68f52566b67516ca2c9eb7ae112d721b851e83
migayalach <ayalachavezmiguel@gmail.com>
2024-06-22 23:34:06 -0400

    params route

--- server/src/routes/paramsRoute.js
+++ server/src/routes/paramsRoute.js
@@ -1,18 +1,18 @@
 const { Router } = require("express");
 const {
   postParams,
-  getParamsAllPage,
   getParamsId,
+  // getParamsAllPage,
   putParams,
-  deleteParams,
+  // deleteParams,
 } = require("../handlers/paramsHandler");
 
 const paramsRouter = Router();
 
-paramsRouter.post("/", postParams);
-paramsRouter.get("/", getParamsAllPage);
-paramsRouter.get("/:idParams", getParamsId);
-paramsRouter.put("/", putParams);
-paramsRouter.delete("/:idParams", deleteParams);
+paramsRouter.post("/", postParams);                   // OK
+paramsRouter.get("/:idUser", getParamsId);            // OK
+paramsRouter.put("/", putParams);                     // EN PROCESO
+// paramsRouter.get("/", getParamsAllPage);
+// paramsRouter.delete("/:idParams", deleteParams);
 
 module.exports = paramsRouter;
