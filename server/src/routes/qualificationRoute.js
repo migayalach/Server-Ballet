4c73884b7575f9493bd2c0119088ae96daad11c0
migayalach <ayalachavezmiguel@gmail.com>
2024-06-22 23:34:20 -0400

    qualification route

--- server/src/routes/qualificationRoute.js
+++ server/src/routes/qualificationRoute.js
@@ -1,17 +1,13 @@
 const { Router } = require("express");
 const {
-  postQualification,
   getQualificationAll,
-  getQualificationId,
-  putQualification,
-  deleteQualification,
+  postQualification,
+  // getQualificationId,
 } = require("../handlers/qualificationHandler");
 const qualificationRouter = Router();
 
-qualificationRouter.post("/", postQualification);
 qualificationRouter.get("/", getQualificationAll);
-qualificationRouter.get("/:idUser", getQualificationId);
-qualificationRouter.put("/", putQualification);
-qualificationRouter.delete("/:idQualification", deleteQualification);
+qualificationRouter.post("/", postQualification);
+// qualificationRouter.get("/:idParams/:idUser", getQualificationId);
 
 module.exports = qualificationRouter;
