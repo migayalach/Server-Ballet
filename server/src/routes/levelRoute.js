2fbe3c3d5c7869dc4066e8fc3d6b7e052a6925e7
migayalach <ayalachavezmiguel@gmail.com>
2024-07-03 16:22:12 -0400

    level route

--- server/src/routes/levelRoute.js
+++ server/src/routes/levelRoute.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
 const {
+  postLevelMiddleware,
+  putLevelMiddleware,
+  deleteLevelMiddleware,
+} = require("../middlewares/levelMiddleware");
+const {
   postLevel,
   getLevelAll,
   getLevelId,
@@ -8,10 +13,10 @@ const {
 } = require("../handlers/levelHandler");
 const levelRouter = Router();
 
-levelRouter.post("/", postLevel);
+levelRouter.post("/", postLevelMiddleware, postLevel);
 levelRouter.get("/", getLevelAll);
 levelRouter.get("/:idLevel", getLevelId);
-levelRouter.put("/", putLevel);
-levelRouter.delete("/:idLevel", deleteLevel);
+levelRouter.put("/", putLevelMiddleware, putLevel);
+levelRouter.delete("/:idLevel", deleteLevelMiddleware, deleteLevel);
 
 module.exports = levelRouter;
