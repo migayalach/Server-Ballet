da7e9bdd4d2ddc8e139fdac6d22778a5c095d273
migayalach <ayalachavezmiguel@gmail.com>
2024-07-04 14:14:35 -0400

    regex

--- server/src/helpers/regex.js
+++ server/src/helpers/regex.js
@@ -1,5 +1,18 @@
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const lengthPassword = /.{8,}/;
+const letterLowerCase = /(?=.*[a-z])/;
+const letterCapitalLetter = /(?=.*[A-Z])/;
+const numberPassword = /(?=.*\d)/;
+const characterSpecial = /(?=.*[@$!%*?&])/;
 
 module.exports = {
   emailRegex,
+  passwordRegex,
+  lengthPassword,
+  letterLowerCase,
+  letterCapitalLetter,
+  numberPassword,
+  characterSpecial,
 };
