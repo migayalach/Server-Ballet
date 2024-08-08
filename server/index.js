d77344ef658c4f5107b9f045bae7df65d172ab6b
migayalach <ayalachavezmiguel@gmail.com>
2024-08-08 00:11:29 -0400

    index js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,5 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server on line in port XD ${PORT}`);
+  console.log(`Server on line in port ${PORT}`);
 });
-
-
-// --watch
