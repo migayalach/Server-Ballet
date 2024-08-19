451762f15cfefdd2ca36c636bfaf027d0544596a
migayalach <ayalachavezmiguel@gmail.com>
2024-08-19 15:39:42 -0400

    contact controller

--- server/src/controllers/contactController.js
+++ server/src/controllers/contactController.js
@@ -1,29 +1,54 @@
 const pool = require("../dataBase/conexion");
 const responseData = require("../utils/response");
+const {
+  getContactAll,
+  existContact,
+} = require("../controllers/controllerData");
 
-const createContact = async (
-  dateContact,
-  nameContact,
-  emailContact,
-  phoneContact
-) => {
-  return `${dateContact} - ${nameContact} - ${emailContact} - ${phoneContact}`;
+const createContact = async (nameContact, emailContact, phoneContact) => {
+  const [ResultSetHeader] = await pool.query(
+    `INSERT INTO sendContact (dateContact, nameContact, emailContact, phoneContact) VALUES (CURRENT_DATE, ?, ?, ?)`,
+    [nameContact, emailContact, phoneContact]
+  );
+  if (!ResultSetHeader) {
+    throw Error(`No se pudo registrar`);
+  }
+  return { message: "Registrado con exito", state: true };
 };
 
 const getAllContact = async () => {
-  return "todo";
+  const data = await getContactAll();
+  if (!data.length) {
+    throw Error`No hay registros disponibles`;
+  }
+  return responseData(data, "contact", (page = 1));
 };
 
 const getPageContact = async (page) => {
-  return `${page}`;
+  const response = await getContactAll();
+  return responseData(response, "contact", page);
 };
 
 const getIdContact = async (idContact) => {
-  return `${idContact}`;
+  const [data] = await pool.query(
+    `SELECT * FROM sendContact WHERE idContact = ?`,
+    [idContact]
+  );
+  if (!data.length) {
+    throw Error`Este contacto no se encuentra registrado`;
+  }
+  return data[0];
 };
 
 const updateContact = async (idContact, stateContact, feedback) => {
-  return `${idContact} ---- ${stateContact} ---- ${feedback}`;
+  if (!(await existContact(idContact))) {
+    throw Error(`No se pudo encontrar el usuario seleccionado`);
+  }
+  await pool.query(
+    `UPDATE sendContact SET stateContact = ?, feedback = ? WHERE idContact = ?`,
+    [stateContact, feedback, idContact]
+  );
+  return await getIdContact(idContact);
 };
 
 module.exports = {
