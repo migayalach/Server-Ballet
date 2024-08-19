9cd36465bb926a631e064c03c9bedf38218200b2
migayalach <ayalachavezmiguel@gmail.com>
2024-08-18 22:23:38 -0400

    list event handler

--- server/src/handlers/listEventHandler.js
+++ server/src/handlers/listEventHandler.js
@@ -4,20 +4,24 @@ const {
   getPageListEvent,
   getIdListEvent,
   updateListEvent,
-  removeListEvent,
 } = require("../controllers/listEventController");
-  
+
 const postListEvent = async (request, response) => {
-  const { dateNews, body, urlPicture } = request.body;
+  const { idHours, dateNews, title, body, urlPicture } = request.body;
   try {
-    const data = await createListEvent(dateNews, body, urlPicture);
+    const data = await createListEvent(
+      idHours,
+      dateNews,
+      title,
+      body,
+      urlPicture
+    );
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
 };
 
-// TODO FILTROS POR FECHA
 const getListEventAll = async (request, response) => {
   const { page } = request.query;
   try {
@@ -39,19 +43,26 @@ const getListEvent = async (request, response) => {
 };
 
 const putListEvent = async (request, response) => {
-  const { idListEvent, dateNews, body, urlPicture } = request.body;
-  try {
-    const data = await updateListEvent(idListEvent, dateNews, body, urlPicture);
-    response.status(200).json(data);
-  } catch (error) {
-    response.status(400).json({ error: error.message });
-  }
-};
+  const {
+    idListEvent,
+    idHours,
+    dateNews,
+    title,
+    body,
+    stateEvent,
+    urlPicture,
+  } = request.body;
 
-const deleteListEvent = async (request, response) => {
-  const { idListEvent } = request.params;
   try {
-    const data = await removeListEvent(idListEvent);
+    const data = await updateListEvent(
+      idListEvent,
+      idHours,
+      dateNews,
+      title,
+      body,
+      stateEvent,
+      urlPicture
+    );
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ error: error.message });
@@ -63,5 +74,4 @@ module.exports = {
   getListEventAll,
   getListEvent,
   putListEvent,
-  deleteListEvent,
 };
