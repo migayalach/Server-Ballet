cbf67eea1f68c468ef238a3e3d1ea24a032292f7
migayalach <ayalachavezmiguel@gmail.com>
2024-09-13 09:38:25 -0400

    extension hanldler

--- server/src/handlers/extensionHandler.js
+++ server/src/handlers/extensionHandler.js
@@ -6,16 +6,7 @@ const {
   removeExtension,
 } = require("../controllers/extensionController");
 
-const postExtension = async (request, response) => {
-  const { nameExtension } = request.body;
-  try {
-    const data = await createExtension(nameExtension);
-    response.status(200).json(data);
-  } catch (error) {
-    response.status(400).json({ error: error.message });
-  }
-};
-
+// TODO: RETORNA LA LISTA COMPLETA DE EXTENSIONES O LOS DATOS PAGINADOS SI SE PROPORCIONA EL PARÁMETRO DE CONSULTA 'page'
 const getExtensionAll = async (request, response) => {
   const { page } = request.query;
   try {
@@ -26,6 +17,18 @@ const getExtensionAll = async (request, response) => {
   }
 };
 
+// TODO: CREAR UNA NUEVA EXTENSION UTILIZANDO 'nameExtension' PROPORCIONADO POR EL BODY DE LA SOLICITUD
+const postExtension = async (request, response) => {
+  const { nameExtension } = request.body;
+  try {
+    const data = await createExtension(nameExtension);
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+// TODO: EDITA UNA EXTENSION UTILIZANDO 'idExtension, nameExtension' PROPORCIONADO POR EL BODY DE LA SOLICITUD
 const putExtension = async (request, response) => {
   const { idExtension, nameExtension } = request.body;
   try {
@@ -36,6 +39,7 @@ const putExtension = async (request, response) => {
   }
 };
 
+// TODO: ELIMINA LA EXTENSION BUSCADO POR 'idExtension' PROPORCIONADO POR EL PARÁMETRO DE LA RUTA
 const deleteExtension = async (request, response) => {
   const { idExtension } = request.params;
   try {
