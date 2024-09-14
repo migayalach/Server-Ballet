63d523bef6bfb838585b193881b073967205b5ea
migayalach <ayalachavezmiguel@gmail.com>
2024-09-14 14:19:26 -0400

    qualification route

--- server/src/routes/qualificationRoute.js
+++ server/src/routes/qualificationRoute.js
@@ -6,12 +6,10 @@ const {
 const {
   getQualificationAll,
   postQualification,
-  // getQualificationId,
 } = require("../handlers/qualificationHandler");
 const qualificationRouter = Router();
 
 qualificationRouter.get("/", validateGetQuaAll, getQualificationAll);
 qualificationRouter.post("/", validatePostQua, postQualification);
-// qualificationRouter.get("/:idParams/:idUser", getQualificationId);
 
 module.exports = qualificationRouter;
