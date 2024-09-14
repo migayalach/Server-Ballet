4939a7e602107ac22b6dffa3da1ce8693c9fae25
migayalach <ayalachavezmiguel@gmail.com>
2024-09-14 13:03:29 -0400

    params handler

--- server/src/handlers/paramsHandler.js
+++ server/src/handlers/paramsHandler.js
@@ -1,12 +1,12 @@
 const {
   createParams,
-  // getAllParams,
-  // getPageParams,
+  getPageParams,
   getIdParams,
   updateParams,
   removeParams,
 } = require("../controllers/paramsController");
 
+// TODO: CREAR UNA NUEVA EVALUACION UTILIZANDO 'idUser, idClass, dateTest, title, params' PROPORCIONADO POR EL BODY DE LA SOLICITUD
 const postParams = async (request, response) => {
   const { idUser, idClass, dateTest, title, params } = request.body;
   try {
@@ -15,8 +15,9 @@ const postParams = async (request, response) => {
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
-}; 
+};
 
+// TODO: RETORNAR TODAS LAS EVALUACIONES BUSCADAS POR 'idUser' EN LOS PARÁMETROS DE LA RUTA
 const getParamsId = async (request, response) => {
   const { idUser } = request.params;
   try {
@@ -27,6 +28,7 @@ const getParamsId = async (request, response) => {
   }
 };
 
+// TODO: EDITA UNA EVALUACION UTILIZANDO 'idParams, idUser, idClass, dateTest, title, params' PROPORCIONADO POR EL BODY DE LA SOLICITUD
 const putParams = async (request, response) => {
   const { idParams, idUser, idClass, dateTest, title, params } = request.body;
   try {
@@ -44,16 +46,18 @@ const putParams = async (request, response) => {
   }
 };
 
-// const getParamsAllPage = async (request, response) => {
-//   const { page } = request.query;
-//   try {
-//     const data = !page ? await getAllParams() : await getPageParams(page);
-//     response.status(200).json(data);
-//   } catch (error) {
-//     response.status(400).json({ error: error.message });
-//   }
-// };
+// TODO: RETORNA LA LISTA COMPLETA DE EVALUACIONES O LOS DATOS PAGINADOS SI SE PROPORCIONA EL PARÁMETRO DE CONSULTA 'idUser, page'
+const getParamsAllPage = async (request, response) => {
+  const { idUser, page } = request.query;
+  try {
+    const data = await getPageParams(idUser, page);
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+};
 
+// TODO: ELIMINA LA EVALUACION BUSCADA POR 'idUser, idParams' PROPORCIONADO POR EL PARÁMETRO DE LA RUTA
 const deleteParams = async (request, response) => {
   const { idUser, idParams } = request.params;
   try {
@@ -69,5 +73,5 @@ module.exports = {
   getParamsId,
   putParams,
   deleteParams,
-  // getParamsAllPage,
+  getParamsAllPage,
 };
