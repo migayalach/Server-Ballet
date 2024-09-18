ebdac3a4c2c71a937022a3e3179ca411b0c27764
migayalach <ayalachavezmiguel@gmail.com>
2024-09-17 22:23:36 -0400

    password middleware

--- server/src/middlewares/passwordMiddleware.js
+++ server/src/middlewares/passwordMiddleware.js
@@ -7,32 +7,34 @@ const {
 } = require("../helpers/regex");
 
 function passwordCases(request, response, next) {
-  const { password } = request.body;
+  const { password, edit } = request.body;
 
-  if (!lengthPassword.test(password)) {
-    return response
-      .status(400)
-      .json({ message: `La contraseña debe tener al menos 8 caracteres` });
-  }
-  if (!letterLowerCase.test(password)) {
-    return response.status(400).json({
-      message: `La contraseña debe tener al menos una letra minúscula`,
-    });
-  }
-  if (!letterCapitalLetter.test(password)) {
-    return response.status(400).json({
-      message: `La contraseña debe tener al menos una letra mayúscula`,
-    });
-  }
-  if (!numberPassword.test(password)) {
-    return response
-      .status(400)
-      .json({ message: `La contraseña debe tener al menos un número` });
-  }
-  if (!characterSpecial.test(password)) {
-    return response.status(400).json({
-      message: `La contraseña debe tener al menos un carácter especial (@, $, !, %, *, ?, &)`,
-    });
+  if (edit === "profile") {
+    if (!lengthPassword.test(password)) {
+      return response
+        .status(400)
+        .json({ message: `La contraseña debe tener al menos 8 caracteres` });
+    }
+    if (!letterLowerCase.test(password)) {
+      return response.status(400).json({
+        message: `La contraseña debe tener al menos una letra minúscula`,
+      });
+    }
+    if (!letterCapitalLetter.test(password)) {
+      return response.status(400).json({
+        message: `La contraseña debe tener al menos una letra mayúscula`,
+      });
+    }
+    if (!numberPassword.test(password)) {
+      return response
+        .status(400)
+        .json({ message: `La contraseña debe tener al menos un número` });
+    }
+    if (!characterSpecial.test(password)) {
+      return response.status(400).json({
+        message: `La contraseña debe tener al menos un carácter especial (@, $, !, %, *, ?, &)`,
+      });
+    }
   }
   next();
 }
