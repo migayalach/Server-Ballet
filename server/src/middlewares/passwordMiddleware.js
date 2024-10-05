4f05ddd5d801412693ae5a6da6de5e2d66af0149
migayalach <ayalachavezmiguel@gmail.com>
2024-10-05 15:07:35 -0400

    password middleware

--- server/src/middlewares/passwordMiddleware.js
+++ server/src/middlewares/passwordMiddleware.js
@@ -5,36 +5,56 @@ const {
   numberPassword,
   characterSpecial,
 } = require("../helpers/regex");
+const { dataId } = require("./funAuxMiddleware");
 
 function passwordCases(request, response, next) {
-  const { password, edit } = request.body;
+  const { idUser, newPassword, oldPassword } = request.body;
 
-  if (edit === "profile") {
-    if (!lengthPassword.test(password)) {
-      return response
-        .status(400)
-        .json({ message: `La contraseña debe tener al menos 8 caracteres` });
-    }
-    if (!letterLowerCase.test(password)) {
-      return response.status(400).json({
-        message: `La contraseña debe tener al menos una letra minúscula`,
-      });
-    }
-    if (!letterCapitalLetter.test(password)) {
-      return response.status(400).json({
-        message: `La contraseña debe tener al menos una letra mayúscula`,
-      });
-    }
-    if (!numberPassword.test(password)) {
-      return response
-        .status(400)
-        .json({ message: `La contraseña debe tener al menos un número` });
-    }
-    if (!characterSpecial.test(password)) {
-      return response.status(400).json({
-        message: `La contraseña debe tener al menos un carácter especial (@, $, !, %, *, ?, &)`,
-      });
-    }
+  if (!dataId(+idUser).state) {
+    return response
+      .status(400)
+      .json({ message: `Por favor ingrese un parametro valido` });
+  }
+
+  if (newPassword === oldPassword) {
+    return response
+      .status(400)
+      .json({ message: `La nueva contraseña no puede ser igual a la antigua` });
+  }
+
+  if (!lengthPassword.test(newPassword) || !lengthPassword.test(oldPassword)) {
+    return response
+      .status(400)
+      .json({ message: `La contraseña debe tener al menos 8 caracteres` });
+  }
+  if (
+    !letterLowerCase.test(newPassword) ||
+    !letterLowerCase.test(oldPassword)
+  ) {
+    return response.status(400).json({
+      message: `La contraseña debe tener al menos una letra minúscula`,
+    });
+  }
+  if (
+    !letterCapitalLetter.test(newPassword) ||
+    !letterCapitalLetter.test(oldPassword)
+  ) {
+    return response.status(400).json({
+      message: `La contraseña debe tener al menos una letra mayúscula`,
+    });
+  }
+  if (!numberPassword.test(newPassword) || !numberPassword.test(oldPassword)) {
+    return response
+      .status(400)
+      .json({ message: `La contraseña debe tener al menos un número` });
+  }
+  if (
+    !characterSpecial.test(newPassword) ||
+    !characterSpecial.test(oldPassword)
+  ) {
+    return response.status(400).json({
+      message: `La contraseña debe tener al menos un carácter especial (., @, $, !, %, *, ?, &)`,
+    });
   }
   next();
 }
