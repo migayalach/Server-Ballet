ccb86632f31f901b76e0245d2e1dab158bf96cd7
migayalach <ayalachavezmiguel@gmail.com>
2024-11-02 23:47:34 -0400

    filter handler

--- server/src/handlers/filterHandler.js
+++ server/src/handlers/filterHandler.js
@@ -1,73 +1,10 @@
-const {
-  filterUser,
-  filterAll,
-  getInfoIdUser,
-  sendFilter,
-} = require("../controllers/filterController");
+const { filterData } = require("../controllers/filterController");
 
 const getFilterUser = async (request, response) => {
-  const {
-    all,
-    search,
-    order,
-    nameOrLastName,
-    idLevel,
-    idExtension,
-    stateUser,
-    totalTime,
-    stateHours,
-    idUser,
-    idTypeClass,
-    stateClass,
-    flag,
-    from,
-    to,
-    stateContact,
-    page,
-  } = request.query;
+  const { search, data, page } = request.query;
   try {
-    switch (flag) {
-      case "send":
-        const data = await sendFilter(
-          flag,
-          from,
-          to,
-          stateContact,
-          (numberPage = page ? page : 1)
-        );
-        return response.status(200).json(data);
-      default:
-        break;
-    }
-
-    // const data = !all
-    //   ? await filterUser(
-    //       all,
-    //       search,
-    //       order,
-    //       nameOrLastName,
-    //       idLevel,
-    //       idExtension,
-    //       stateUser,
-    //       totalTime,
-    //       stateHours,
-    //       idUser,
-    //       idTypeClass,
-    //       stateClass,
-    //       page
-    //     )
-    //   : await filterAll(all);
-    // response.status(200).json(data);
-  } catch (error) {
-    response.status(400).json({ error: error.message });
-  }
-};
-
-const getIdUserData = async (request, response) => {
-  const { idUser } = request.params;
-  try {
-    const data = await getInfoIdUser(idUser);
-    response.status(200).json(data);
+    const dataResponse = await filterData(search, JSON.parse(data), page);
+    return response.status(200).json(dataResponse);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
@@ -75,5 +12,4 @@ const getIdUserData = async (request, response) => {
 
 module.exports = {
   getFilterUser,
-  getIdUserData,
 };
