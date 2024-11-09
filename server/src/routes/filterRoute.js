82cf244a16634d2ff9911f3e6ea8c4592f46b984
migayalach <ayalachavezmiguel@gmail.com>
2024-11-09 12:01:58 -0400

    filter route

--- server/src/routes/filterRoute.js
+++ server/src/routes/filterRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getFilterUser, getIdUserData } = require("../handlers/filterHandler");
+const { getFilterUser } = require("../handlers/filterHandler");
 const filterRouter = Router();
 
 filterRouter.get("/", getFilterUser);
