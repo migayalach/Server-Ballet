f5ea24f232bf629da3136f99b0e9cc3b09e754f1
migayalach <ayalachavezmiguel@gmail.com>
2024-11-20 21:58:05 -0400

    class student route

--- server/src/routes/classStudentRoute.js
+++ server/src/routes/classStudentRoute.js
@@ -3,28 +3,21 @@ const { Router } = require("express");
 const {
   validategetIdClassStu,
   validatePostClassStu,
-  validateDeleteClassStu,
 } = require("../middlewares/classStudentMiddleware");
 
 const {
   getClassStudentAll,
   getClassStudentId,
   postClassStudent,
-  deleteClassStudent,
+  putClassStudent,
 } = require("../handlers/classStudentHandler");
 
 const classStudentRoute = Router();
 
 // TODO ESTA RUTA MUESTRA TODO EL LISTADO DE ESTUDIANTES POR CLASE
 classStudentRoute.get("/:idClass", validategetIdClassStu, getClassStudentId);
-
 classStudentRoute.get("/", getClassStudentAll);
 classStudentRoute.post("/", validatePostClassStu, postClassStudent);
-// TODO FALTA REALIZAR DELETE EN EL FRONTEND
-classStudentRoute.delete(
-  "/:idClass/:idUser",
-  validateDeleteClassStu,
-  deleteClassStudent
-);
+classStudentRoute.put("/", validatePostClassStu, putClassStudent);
 
 module.exports = classStudentRoute;
